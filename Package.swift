1ef06c895933049628ffaf70d1a24d69147e6be1
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-23 10:55:14 -0400

    Add transpiled bridged types for AuthSignUpResult

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
 // This is a Skip (https://skip.tools) package,
 // containing a Swift Package Manager project
 // that will use the Skip build plugin to transpile the
